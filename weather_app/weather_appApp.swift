5b1553b333c8bde373cc6bc444b0dcbc5b9a7db7
MarquesThomasCoding <thomasmarques.lucky@gmail.com>
2025-11-04 09:18:03 +0100

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by MARQUES Thomas on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather_appTests/weather_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  weather_appTests.swift
+//  weather_appTests
+//
+//  Created by MARQUES Thomas on 04/11/2025.
+//
+
+import Testing
+
+struct weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
